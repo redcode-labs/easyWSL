084672e4f7f3b0a35d86407738afdda0bb38b351
Łukasz Jendrysik <scadu9@gmail.com>
2021-09-09 08:54:48 +0200

    Ensure post-install script is POSIX compatible
    Do not use Bash-specific features that will fail in distributions like Alpine Linux.

--- easyWSL/post-install.sh
+++ easyWSL/post-install.sh
@@ -1,34 +1,35 @@
+#!/usr/bin/env sh
 echo "Upgrading the system ..."
-if command -v apt &> /dev/null; then
-  apt-get -y update &> /dev/null
-  apt-get -y install sudo &> /dev/null
+if command -v apt > /dev/null 2>&1; then
+  apt-get -y update > /dev/null 2>&1
+  apt-get -y install sudo > /dev/null 2>&1
 fi
 
-if command -v pacman &> /dev/null; then
-  pacman -Syu --noconfirm &> /dev/null
-  pacman -S --noconfirm sudo &> /dev/null
+if command -v pacman > /dev/null 2>&1; then
+  pacman -Syu --noconfirm > /dev/null 2>&1
+  pacman -S --noconfirm sudo > /dev/null 2>&1
 fi
 
-if command -v apk &> /dev/null; then
-  apk update &> /dev/null
-  apk add sudo &> /dev/null
+if command -v apk > /dev/null 2>&1; then
+  apk update > /dev/null 2>&1
+  apk add sudo shadow > /dev/null 2>&1
 fi
 
-if command -v rpm &> /dev/null; then
-  rpm -U &> /dev/null
-  rpm -i sudo &> /dev/null
+if command -v rpm > /dev/null 2>&1; then
+  rpm -U > /dev/null 2>&1
+  rpm -i sudo > /dev/null 2>&1
 fi
 
-if command -v xbps &> /dev/null; then
-  xbps-install -Su &> /dev/null
-  xbps-install sudo &> /dev/null
+if command -v xbps > /dev/null 2>&1; then
+  xbps-install -Su > /dev/null 2>&1
+  xbps-install sudo > /dev/null 2>&1
 fi
 
-if command -v emerge &> /dev/null; then
-  emaint -a sync &> /dev/null
-  emerge-webrsync &> /dev/null
-  eix-sync &> /dev/null
-  emerge -a sudo &> /dev/null
+if command -v emerge > /dev/null 2>&1; then
+  emaint -a sync > /dev/null 2>&1
+  emerge-webrsync > /dev/null 2>&1
+  eix-sync > /dev/null 2>&1
+  emerge -a sudo > /dev/null 2>&1
 fi
 
 add_user()
@@ -40,7 +41,7 @@ add_user()
     echo Try again.
   done
   printf "$USERNAME ALL=(ALL:ALL) ALL" >> /etc/sudoers
-  printf "%s\n" "[user]" "default=$USERNAME" >> /etc/wsl.conf 
+  printf "%s\n" "[user]" "default=$USERNAME" >> /etc/wsl.conf
 }
 
 set_root_passwd()
@@ -60,10 +61,11 @@ chmod 755 /bin/su
 chmod u+s /bin/su
 
 while true; do
-  read -p "Do you want to create a new user with administrator privileges? [y/n]: " yn
-  case $yn in
-    [Yy]* ) add_user; break;;
-    [Nn]* ) set_root_passwd; break;;
-  esac
+  echo -n "Do you want to create a new user with administrator privileges? [y/n]: "
+  read answer
+  if [ "$answer" != "${answer#[Yy]}" ]; then
+    add_user
+  else
+    set_root_passwd
+  fi
 done
-
